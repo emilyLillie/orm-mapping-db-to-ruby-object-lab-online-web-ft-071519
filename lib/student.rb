66fcd95913415549fd415f3d54aff69c561e9d74
Emily Lillie <emilylillie3@gmail.com>
2019-08-17 16:17:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,8 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE name = ? LIMIT 1 
     SQL
-    DB[:conn].execute(sql, name).map do |
+    DB[:conn].execute(sql, name).map do |row|
+      
   end
   
   def save
