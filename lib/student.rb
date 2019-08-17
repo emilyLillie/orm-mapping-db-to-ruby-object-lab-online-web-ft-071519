c0fe79022bf456955da1dba125848357e1809080
Emily Lillie <emilylillie3@gmail.com>
2019-08-17 16:21:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,9 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE students.grade = 9 
     SQL
+    DB[:conn].execute(sql).collect do |row|
+    self.new_from_db(row)
+  end
   
   
   
