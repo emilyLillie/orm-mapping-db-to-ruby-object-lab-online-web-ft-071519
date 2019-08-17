17e3f1b848cff746730f051a767eabc6b90c413d
Emily Lillie <emilylillie3@gmail.com>
2019-08-17 16:34:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,7 @@ class Student
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
     SELECT * FROM students WHERE students.grade = 10 
+    ORDER BY students.id
     LIMIT ?
     SQL
     DB[:conn].execute(sql).collect do |row| 
